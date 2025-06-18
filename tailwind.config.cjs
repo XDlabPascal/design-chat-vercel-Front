ee3d8d178e70d2d778832b427ea20b2e6d64421c
XDLab Pascal <153315502+XDlabPascal@users.noreply.github.com>
2025-06-18 09:48:11 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['ui-sans-serif', 'system-ui'],
+      },
+    },
   },
   plugins: [],
-}
+};
