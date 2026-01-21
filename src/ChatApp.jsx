551e9b20fa4dfaf6380b88f8989830ad14cbec2a
XDLab Pascal <153315502+XDlabPascal@users.noreply.github.com>
2026-01-21 13:41:48 +0100

    Update greeting message in ChatApp component

--- src/ChatApp.jsx
+++ src/ChatApp.jsx
@@ -20,7 +20,7 @@ export default function ChatApp() {
     {
       role: 'assistant',
       content:
-        "Bonjour !\n\nJe suis Lucas, un agent IA, imaginé par Sophie Arsac et Pascal Jambie, pour évaluer tes connaissances sur le design, \net plus généralement sur la conception centrée utilisateur. \nJe vais te poser 10 questions.\nN’hésite pas à répondre franchement, tu peux aussi répondre « je ne sais pas ». \nPrêt·e ? \nDis moi Ok quand tu l’ai \n",
+        "Bonjour !\nJe suis Lucas, un agent IA, imaginé par Sophie Arsac et Pascal Jambie, pour évaluer tes connaissances sur le design, \net plus généralement sur la conception centrée utilisateur. \nJe vais te poser 10 questions.\nN’hésite pas à répondre franchement, tu peux aussi répondre « je ne sais pas ». \nDis moi Ok quand tu es prêt(e) \n",
     },
   ]);
   const [input, setInput] = useState('');
